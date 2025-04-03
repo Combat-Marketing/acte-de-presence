250728f87ccef769595e9418dae0bd2691411666
Christiaan Benedictus <chris@combat.nl>
2025-04-03 23:08:44 +0200

    Setting up the basics to start with authjs and next-auth 5

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -55,7 +55,9 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                <main className="flex flex-col min-h-screen">
+                    {children}
+                </main>
             </body>
         </html>
     );
