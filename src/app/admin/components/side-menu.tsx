0097f23f850a38e5fae8c06b5be46000c0a08f49
Christiaan Benedictus <chris@combat.nl>
2025-04-05 10:02:06 +0200

    Added notification button and badge support

--- src/app/admin/components/side-menu.tsx
+++ src/app/admin/components/side-menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArchiveBoxIcon, MegaphoneIcon, ChartBarIcon, Cog6ToothIcon, UserIcon, ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import { ArchiveBoxIcon, MegaphoneIcon, ChartBarIcon, Cog6ToothIcon, UserIcon, ArrowLeftStartOnRectangleIcon, BellIcon } from "@heroicons/react/24/outline";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
@@ -25,6 +25,7 @@ export default function SideMenu() {
                     <MenuButton id="settings" icon={<Cog6ToothIcon />} disabled={true} label="Settings" onClick={() => { }} />
                 </div>
                 <div className="mt-auto p-1 flex flex-col gap-1">
+                    <MenuButton id="notifications" icon={<BellIcon />} disabled={false} badge={1} label="Notifications" onClick={() => { }} />
                     <MenuButton id="profile" icon={<UserIcon />} disabled={true} label="Profile" onClick={() => { }} />
                     <MenuButton id="logout" icon={<ArrowLeftStartOnRectangleIcon />} disabled={false} label="Logout" onClick={() => setShowLogoutPopup(true)} />
                 </div>
@@ -46,12 +47,15 @@ export default function SideMenu() {
     )
 }
 
-const MenuButton = ({ id, icon, label, disabled, onClick }: { id: string, icon: React.ReactNode, label: string, disabled?: boolean, onClick: () => void }) => {
+const MenuButton = ({ id, icon, label, disabled, badge, onClick }: { id: string, icon: React.ReactNode, label: string, disabled?: boolean, badge?: number, onClick: () => void }) => {
     return (
         <button type="button" id={id} onClick={onClick} disabled={disabled} className={`group relative w-10 h-10 flex justify-center items-center text-white rounded-md p-2 transition-colors duration-300 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-[var(--color-bg-primary)] active:bg-[var(--color-bg-primary)]'}`}>
             {icon}
             {!disabled && <div className="absolute left-14 bg-[var(--color-bg-primary)] text-white px-2 py-1 rounded-md text-sm text-left opacity-0 pointer-events-none group-hover:opacity-100 transition-opacity duration-300">
                 {label}
+            </div>}
+            {badge && <div className="absolute -top-1 -right-1 bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full text-xs">
+                {badge}
             </div>}
         </button>
     )
